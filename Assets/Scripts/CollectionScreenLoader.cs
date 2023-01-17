86c0ae57d57467a76c1e84d1cf8d7e635753e1a2
Levi van Boxtel <le.van.boxtel@sintlucasedu.nl>
2023-01-17 09:21:00 +0100

    Start SceneLoader

---
+++ Assets/Scripts/CollectionScreenLoader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CollectionScreenLoader : MonoBehaviour
+{
+    
+    
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
